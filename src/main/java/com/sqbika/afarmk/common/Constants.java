6d65e0272366b008b69945af0971293b4841ab1a
Sqbika <sqbika@sqbika.win>
2020-03-29 12:32:33 +0200

    Version bump in constants

--- src/main/java/com/sqbika/afarmk/common/Constants.java
+++ src/main/java/com/sqbika/afarmk/common/Constants.java
@@ -4,7 +4,7 @@ package com.sqbika.afarmk.common;
 public class Constants {
     public static final String MOD_ID = "afarmk";
     public static final String MOD_NAME = "AFarmK";
-    public static final String MOD_VERSION = "0.3";
+    public static final String MOD_VERSION = "0.4";
 
     public static final String CLIENT_PROXY = "com.sqbika.afarmk.ClientProxy";
     public static final String COMMON_PROXY = "com.sqbika.afarmk.CommonProxy";
